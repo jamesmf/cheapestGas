61c40629162c248ef3f1de7f45de2c1b3130abaf
jamesmf <jamesmfrick@gmail.com>
2017-04-22 12:11:15 -0400

    add timing, remove printing, check solution

--- src/CheapestGas.java
+++ src/CheapestGas.java
@@ -1,5 +1,6 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.Collections;
@@ -25,15 +26,16 @@ public class CheapestGas {
 	
 	
 	public static void main(String args[]) throws IOException{
-		int[] Ns = {25}; //number of stations
+		int[] Ns = {2500000,5000000,10000000,15000000}; //number of stations
 		int U = 5; //size of the tank -- can vary
 		
-		FileWriter writer = new FileWriter("../output.csv");
+		FileWriter writer = new FileWriter("output.csv");
 		
 		for (int n : Ns){
 			ArrayList<Station> data = getData(n);
-			System.out.println(data);
+//			System.out.println(data);
 			
+			//initialize the time
 			double t1 = System.currentTimeMillis();
 			
 			//initialize our objects to slide a window of 'distance' U (tank size) over the nodes
@@ -44,8 +46,10 @@ public class CheapestGas {
 			ArrayList<Station> prev = new ArrayList<Station>(); //list of cheapest previous station within range U for station i
 			ArrayList<Station> next = new ArrayList<Station>(); //list of cheapest future station within range U for station i
 			for (int i=0; i< n; i++){
-				prev.add(new Station(-1,10000000,10000000,10000000)); //initialize with unimportant values
-				next.add(new Station(-1,10000000,10000000,10000000));
+				//initialize with dummy values
+				Station blankStation = new Station(-1,10000000,10000000,10000000);
+				prev.add(blankStation); 
+				next.add(blankStation);
 			}
 
 			//iterate over the stations to calculate 'next' and 'prev' cheapest stations ONCE (dynamic programming)
@@ -58,7 +62,7 @@ public class CheapestGas {
 						//the minimum cost right after we remove station i is next(i) 
 						//because when it is the earliest station left in the queue, every other station in the queue is 
 						//1) within driving distance U and 2) after it
-						System.out.println("before adding station "+s.index+" we remove " +prevS.index);
+//						System.out.println("before adding station "+s.index+" we remove " +prevS.index);
 						underConsideration.remove(0);
 						window.remove(prevS);
 						Station nextStation = Collections.min(underConsideration,new StationComparator());
@@ -66,9 +70,6 @@ public class CheapestGas {
 						next.set(prevS.index, nextStation);
 
 					}
-/*					else if (prevS.distanceFromZero + U > data.get(data.size()-1).distanceFromZero){
-						break;
-					}*/
 					else{
 						break;
 					}
@@ -77,9 +78,9 @@ public class CheapestGas {
 				//insert into the priority queue
 				window.add(s);
 				underConsideration.add(s);
-				//immediately when the station falls into the window, we can calculate prev(i)
+				//immediately when the station falls into the window, we can calculate prev
 				Station prevStation = Collections.min(underConsideration,new StationComparator());
-				//assign minimum cost station under consideration as the 'prev()' of station s
+				//assign minimum cost station under consideration as the 'prev[s]' of station s
 				prev.set(s.index, prevStation);
 				
 			}
@@ -87,16 +88,16 @@ public class CheapestGas {
 			//so here we have to process remaining elements of the queue
 			while(true && underConsideration.size() > 0){
 				Station s2 = underConsideration.get(0);
-				System.out.println("before we finish, remove "+s2.index);
+//				System.out.println("before we finish, remove "+s2.index);
 				Station nextStation = Collections.min(underConsideration,new StationComparator());
 				next.set(s2.index, nextStation);
 				underConsideration.remove(0);
 				window.remove(s2);
 			}
-			System.out.println("Array of Cheapest Gas Station Previous to Station i Within Distance U");
-			System.out.println(prev);
-			System.out.println("Array of Cheapest Gas Station After Station i Within Distance U");
-			System.out.println(next);
+//			System.out.println("Array of Cheapest Gas Station Previous to Station i Within Distance U");
+//			System.out.println(prev);
+//			System.out.println("Array of Cheapest Gas Station After Station i Within Distance U");
+//			System.out.println(next);
 			
 			
 			//now we need to check which stations are "breakpoints" - where prev[i] == i
@@ -108,50 +109,62 @@ public class CheapestGas {
 				}
 				i++;
 			}
-			System.out.println(breakPoints);
+//			System.out.println(breakPoints);
 			
 			//now we can 'paste together' solutions from breakpoint to breakpoint to attain the global optimum
 			breakPoints.add(n-1);
 			breakPoints.remove(0);
-			int j = 0;
-			int k = 0;
+			int[] jk = {0,0};
 			int counter = 0;
 			double totalCost = 0;
 			
-			while (k < n-1){
-				k = breakPoints.get(counter);
-				System.out.println("outside: "+j+" "+k);
-				driveToNext(j,k,data,next,totalCost,U);
-				j = k;
+			while (jk[1] < n-1){
+				jk[1] = breakPoints.get(counter);
+//				System.out.println(jk[0]+" "+jk[1]);
+				totalCost = driveToNext(jk,data,next,totalCost,U);
 				counter++;
 			}
-			System.out.println(totalCost);
+//			System.out.println(totalCost);
+			double time = System.currentTimeMillis() - t1;
+			writer.write(String.valueOf(n)+','+n*Math.log(n)+','+time);
+			System.out.println(String.valueOf(n)+','+n*Math.log(n)+','+time);
 		}
+	writer.close();
 	}
 
 
-	private static void driveToNext(int j, int k, ArrayList<Station> data, ArrayList<Station> next, double totalCost, int U) {
+	private static double driveToNext(int[] jk, ArrayList<Station> data, ArrayList<Station> next, double totalCost, int U) {
 		//this implements the locally optimal solution that, when used only from point i to a breakpoint k, achieves global optimization
+		DecimalFormat df = new DecimalFormat("#.000"); 
+		int j = jk[0];
+		int k = jk[1];
 		Station stationJ = data.get(j);
 		Station stationK = data.get(k);
-		int failsafe = 0;
-		while (j < k &&  failsafe <10 ){
+		while (j < k ){
 			if (stationK.distanceFromZero - stationJ.distanceFromZero > U){
+				//this means we can't drive straight to k, we need to stop at an intermediate station
 				Station upNext = next.get(j);
 				j = upNext.index;
-				totalCost += stationJ.cost * (upNext.distanceFromZero - stationJ.distanceFromZero); 
-				System.out.println(upNext);
+				double diff = (upNext.distanceFromZero - stationJ.distanceFromZero);
+				double c = stationJ.cost * diff;
+//				System.out.println("drove from "+stationJ.index+", a distance of "+df.format(diff)+", to "+upNext.index+"; cost "+df.format(stationJ.cost)+"*"+df.format(diff)+'='+df.format(stationJ.cost*diff));
+				totalCost +=  c;
 				stationJ = data.get(j);
+				
 			}
 			else{
+				//this means we can drive straight to breakpoint k
 				Station upNext = stationK;
 				j = upNext.index;
-				totalCost += stationJ.cost * (upNext.distanceFromZero - stationJ.distanceFromZero);
-				System.out.println(upNext);
+				double diff = (upNext.distanceFromZero - stationJ.distanceFromZero);
+				double c = stationJ.cost * diff;
+//				System.out.println("drove from "+stationJ.index+", a distance of "+df.format(diff)+", to "+upNext.index+"; cost "+df.format(stationJ.cost)+"*"+df.format(diff)+'='+df.format(stationJ.cost*diff));
+				totalCost += c;
 			}
-			System.out.println("inside: "+j+" "+k+" "+totalCost);
-			failsafe++;
 		}
+		jk[0] = j;
+		jk[1] = k;
+		return totalCost;
 		
 	}
 	
